f631ee76e6f84d98f5140ff99d80096f8c1dc457
Warley <warley.alves@onblox.com.br>
2023-06-19 21:42:01 -0300

    teste maxima init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ClientNewComponent } from './pages/client-new/client-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomepageComponent },
+  { path: "cliente/novo", component: ClientNewComponent },
+  { path: "**", component: ClientNewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ClientNewComponent } from './pages/client-new/client-new.component';
+import { ChartMapComponent } from './components/chart-map/chart-map.component';
+import { ClientDatatableComponent } from './components/client-datatable/client-datatable.component';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    ClientNewComponent,
+    ChartMapComponent,
+    ClientDatatableComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
